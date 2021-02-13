f8f6005dca55a1d62d8f4a8c586b50c20e0a49bd
ashill99 <admshng@gmail.com>
2021-02-13 18:42:22 -0500

    befoe fonts

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,4 +1,4 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :name, :address, :restOrBar, :restType, :rating, :longitude, :latitude, :id, :imgUrl, :menu, :website, :hours
+  attributes :name, :address, :restOrBar, :restType, :rating, :longitude, :latitude, :id, :imgUrl, :menu, :website, :hours, :comments
   has_many :comments, :favourites 
 end
